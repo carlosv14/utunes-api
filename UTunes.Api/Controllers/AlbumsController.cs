81aa53bc4624f5868799a4ef768e1518b05d7dc1
cvare <cvarela1496@outlook.com>
2023-05-15 12:49:09 -0600

    Add project

---
+++ UTunes.Api/Controllers/AlbumsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UTunes.Core;
+using UTunes.Core.Entities;
+
+namespace UTunes.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AlbumsController : ControllerBase
+{
+    private readonly IRepository<Album> repository;
+
+    public AlbumsController(IRepository<Album> repository)
+    {
+        this.repository = repository;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAlbums() => Ok(await this.repository.AllAsync());
+}
+
---
+++ UTunes.Core/Entities/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace UTunes.Core.Entities
+{
+	public class Album
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Artist { get; set; }
+
+		public string Review { get; set; }
+
+		public ICollection<Song> Songs { get; set; }
+	}
+}
+
